debcc72656479faf6de29dda7c54b0d104a5c9f9
Admin-PC\Admin <pil717171@gmail.com>
2019-03-13 15:31:11 +0300

    (lesson3) -> modify tests and main.js

--- 3/3.2/main.js
+++ 3/3.2/main.js
@@ -1,91 +1,104 @@
 // var button = document.querySelector('button');
 // button.onclick = profile;
 
-	var name, surname, lastname, age, old, day, sex, pen;
-	/* ф-я получения имени. При введенной пустой строке снова вызывает себя*/
-	function getName () {
-		name = prompt('Введите Ваше имя');
-		if (name == '' || name == null) {
-			getName();
-		}
-		else {
-			getSurname();
-		}
+var name, surname, lastname, age, old, day, sex, pen;
+/* ф-я получения имени. При введенной пустой строке снова вызывает себя*/
+function getName () {
+	name = prompt('Введите Ваше имя');
+	if (name == '' || name == null) {
+		// getName();
+		return null;
 	}
-	/* ф-я получения отчества. При введенной пустой строке снова вызывает себя*/
-	function getSurname () {
-		surname = prompt('Введите Ваше отчество');
-		if (surname == '' || name == null) {
-			getSurname();
-		}
-		else {
-			getLastname();
-		}
+	else {
+		// getSurname();
+		return name;
 	}
-	/* ф-я получения фамилии. При введенной пустой строке снова вызывает себя*/
-	function getLastname () {
-		lastname = prompt('Введите Вашу фамилию');
-		if (lastname == '' || name == null) {
-			getLastname();
-		}
-		else {
-			getAge();
-		}
+}
+/* ф-я получения отчества. При введенной пустой строке снова вызывает себя*/
+function getSurname () {
+	surname = prompt('Введите Ваше отчество');
+	if (surname == '' || name == null) {
+		// getSurname();
+		return null;
 	}
-	/* ф-я получения количества лет.С помощью регулярного выражения проверяет введены ли цифры в поле. Если в поле имеются буквы либо возраст
+	else {
+		// getLastname();
+		return surname;
+	}
+}
+/* ф-я получения фамилии. При введенной пустой строке снова вызывает себя*/
+function getLastname () {
+	lastname = prompt('Введите Вашу фамилию');
+	if (lastname == '' || name == null) {
+		// getLastname();
+		return null;
+	}
+	else {
+		// getAge();
+		return lastname;
+	}
+}
+/* ф-я получения количества лет.С помощью регулярного выражения проверяет введены ли цифры в поле. Если в поле имеются буквы либо возраст
 нереальный снова вызывает себя. В противном случае вызывает следующую ф-ю*/
-	function getAge () {
-		age = prompt('Сколько Вам лет?');
-		if (age.replace (/\d/g, '').length || parseInt(age) > 130 || parseInt(age) < 0 || age == '') {
-			getAge();
-		}
-		else {
-			getOldDay(age);
-		}
+function getAge () {
+	age = prompt('Сколько Вам лет?');
+	if (age.replace (/\d/g, '').length || parseInt(age) > 130 || parseInt(age) < 0 || age == '') {
+		// getAge();
+		return null;
 	}
-	/* ф-я подсчета граф: кол-во лет через 5 лет и возраст в днях*/
-	function getOldDay (arg) {
-		arg = parseInt(arg);
-		old = arg + 5;
-		day = arg*365;
-		getSex();
+	else {
+		// getOldDay(age);
+		return age;
 	}
-	/* ф-я определения пола опрашиваемого. При утвердитнльном ответе переход в функию подсчета для мужчин, в противном случае - для женщин*/
-	function getSex () {
-		sex = confirm('Вы мужчина?');
-		if (!sex) {
-			sex = 'женский';
-			getPenWoman();
+}
+/* ф-я подсчета граф: кол-во лет через 5 лет и возраст в днях*/
+function getOldDay (arg) {
+	arg = parseInt(arg);
+	old = arg + 5;
+	day = arg*365;
+	return {old : old, day : day};
+	// getSex();
+}
+/* ф-я определения пола опрашиваемого. При утвердитнльном ответе переход в функию подсчета для мужчин, в противном случае - для женщин*/
+function getSex () {
+	sex = confirm('Вы мужчина?');
+	if (!sex) {
+		sex = 'женский';
+		// getPenWoman();
+		
 
-		}
-		else {
-			sex = 'мужской';
-			getPenMan();
-		}
 	}
-	/* ф-я подсчета пенсионного возраста мужчин*/
-	function getPenMan () {
-		if (age > 68) {
-			pen = 'да';
-		}
-		else {
-			pen = 'нет';
-		}
+	else {
+		sex = 'мужской';
+		// getPenMan();
 	}
-	/* ф-я подсчета пенсионного возраста женщин*/
-	function getPenWoman () {
-		if (age > 63) {
-			pen = 'да';
-		}
-		else {
-			pen = 'нет';
-		}
+	return sex;
+}
+/* ф-я подсчета пенсионного возраста мужчин*/
+function getPenMan () {
+	if (age > 68) {
+		pen = 'да';
 	}
-	/* ф-я вывода полученных данных*/
-	function prof () {
-		getName();
-		alert('Ваши ФИО:' + ' ' + lastname + ' '  + name + ' ' + surname +'\n' + 'Ваш возраст в годах:' + ' ' + age + '\n'
+	else {
+		pen = 'нет';
+	}
+	return pen;
+}
+/* ф-я подсчета пенсионного возраста женщин*/
+function getPenWoman () {
+	if (age > 63) {
+		pen = 'да';
+	}
+	else {
+		pen = 'нет';
+	}
+	return pen;
+}
+/* ф-я вывода полученных данных*/
+function prof () {
+	getName();
+	alert('Ваши ФИО:' + ' ' + lastname + ' '  + name + ' ' + surname +'\n' + 'Ваш возраст в годах:' + ' ' + age + '\n'
         + 'Ваш возраст в днях:' + ' ' + day + '\n' + 'Через 5 лет Вам будет:' + ' ' + old + '\n' + 'Ваш пол:' + ' '
         + sex + '\n' + 'Вы на пенсии:' + ' ' + pen);
-	}
+}
 	
--- 3/3.2/main.spec.js
+++ 3/3.2/main.spec.js
@@ -1,17 +1,131 @@
-
-
-
-
-xdescribe('2 -> 2.2 -> getName', function() {
-    it('should work with null', function() {
-        let isFirstCall = false;
-        spyOn(window, 'prompt').and.callFake(function () {
-            let result = isFirstCall ? 'vasia' : null;
-            isFirstCall = true;
-            return result;
-                });
-        result = getName();
-        expect(result).toEqual(getName);
-        expect(result).toEqual(getSurname);
-    });
+describe('3.2 -> getName', function() {
+	it('should behave function getName to return string name', function () {
+		let name = 'vasia';
+		spyOn(window, 'prompt').and.returnValue(name);
+		expect(getName()).toBe(name);
+	});
+	it('should behave function getName to return null', function () {
+		let name = '';
+		spyOn(window, 'prompt').and.returnValue(name);
+		expect(getName()).toEqual(null);
+	});
 });
+
+
+describe('3.2 -> getSurname', function() {
+	it('should behave function getSurname to return string surname', function() {
+		let surname = 'ivanovich';
+		spyOn(window, 'prompt').and.returnValue(surname);
+		expect(getSurname()).toBe(surname);
+	});
+	it('should behave function getSurname to return null', function () {
+		let surname = '';
+		spyOn(window, 'prompt').and.returnValue(surname);
+		expect(getSurname()).toEqual(null);
+	});	
+});
+	
+describe('3.2 -> getLastname', function() {
+	it('should behave function getLastname to return string lastname', function() {
+		let lastname = 'petrow';
+		spyOn(window, 'prompt').and.returnValue(lastname);
+		expect(getLastname()).toBe(lastname);
+	});
+	it('should behave function getLastname to return null', function () {
+		let lastname = '';
+		spyOn(window, 'prompt').and.returnValue(lastname);
+		expect(getLastname()).toEqual(null);
+	});	
+});	
+
+describe('3.2 -> getAge', function() {
+	it('should behave function getAge to return age', function() {
+		let age = '40';
+		spyOn(window, 'prompt').and.returnValue(age);
+		expect(getAge()).toBe(age);
+	});
+	it('should behave function getAge to return null', function () {
+		let age = '';
+		spyOn(window, 'prompt').and.returnValue(age);
+		expect(getAge()).toEqual(null);
+	});	
+	it('should behave function getAge > 130 to return null', function () {
+		let age = '150';
+		spyOn(window, 'prompt').and.returnValue(age);
+		expect(getAge()).toEqual(null);
+	});	
+	it('should behave function getAge < 0 to return null', function () {
+		let age = '-10';
+		spyOn(window, 'prompt').and.returnValue(age);
+		expect(getAge()).toEqual(null);
+	});	
+	it('should behave function getAge no number to return null', function () {
+		let age = 'ten';
+		spyOn(window, 'prompt').and.returnValue(age);
+		expect(getAge()).toEqual(null);
+	});	
+});	
+
+describe('3.2 -> getOldDay', function() {
+	it('should behave function getOldDay to return days', function() {
+		expect(getOldDay('10')).toEqual({old: 15, day: 3650});
+	});
+});	
+
+describe('3.2 -> getSex', function() {
+	it('should behave function getSex to push ok return string men', function() {
+		let conf = true;
+		spyOn(window, 'confirm').and.returnValue(conf);
+		expect(getSex()).toBe(sex = 'мужской');
+	});
+	it('should behave function getSex to push cancel return string women', function() {
+		let conf = false;
+		spyOn(window, 'confirm').and.returnValue(conf);
+		expect(getSex()).toBe(sex = 'женский');
+	});
+});	
+
+describe('3.2 -> getPenMan', function() {
+	let age;
+	beforeEach(function() {
+		age = '80';
+		getAge(spyOn(window, 'prompt').and.returnValue(age));
+	});
+	it('should behave function getPenMan to age > 68 ok return pen = Да', function() {
+		expect(getPenMan()).toBe(pen = 'да');
+	});
+});	
+
+describe('3.2 -> getPenMan', function() {
+	let age;
+	beforeEach(function() {
+		age = '30'
+		getAge(spyOn(window, 'prompt').and.returnValue(age));
+	});
+	it('should behave function getPenMan to age < 68 ok return pen = нет', function() {
+		expect(getPenMan()).toBe(pen = 'нет');
+	});
+});	
+
+describe('3.2 -> getPenWoman', function() {
+	let age;
+	beforeEach(function() {
+		age = '64';
+		getAge(spyOn(window, 'prompt').and.returnValue(age));
+	});
+	it('should behave function getPenMan to age > 63 ok return pen = Да', function() {
+		expect(getPenWoman()).toBe(pen = 'да');
+	});
+});	
+
+describe('3.2 -> getPenWoman', function() {
+	let age;
+	beforeEach(function() {
+		age = '30';
+		getAge(spyOn(window, 'prompt').and.returnValue(age));
+	});
+	it('should behave function getPenMan to age < 63 ok return pen = нет', function() {
+		expect(getPenWoman()).toBe(pen = 'нет');
+	});
+});	
+
